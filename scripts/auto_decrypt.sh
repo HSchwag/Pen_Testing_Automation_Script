ad67bb80f7eebe6f4c328e3a2d6fb604f92324b3
Hunter Schwager <hunterschwager@gmail.com>
2025-06-06 22:06:14 +0000

    Django Ready: Basic Scan

--- scripts/auto_decrypt.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# This is meant to decrpyt pulled git hub projects.
-
-openssl enc -pbkdf2 -nosalt -aes-256-cbc -in /home/bob/bootcon/bootcon_project_enc.py -out /home/bob/bootcon/bootcon_project_dec.py -d -base64 -K 4EA2A56396E4BB2C30897C1B8C090F54E8F2E1308780B8795E6B53A6E74FD814 -iv E69CCC163DA315AC46D66149421B2658
-
-echo [SUCCESS]File Decrypted.
