aa2484cd9228d70179f6723630739564655fadf7
Hunter Schwager <hunterschwager@gmail.com>
2025-06-10 23:08:49 -0600

    Update draft1.py

--- draft1.py
+++ draft1.py
@@ -63,10 +63,10 @@ def mosquito_in_amber():
 
 def steg_command(to_be_hidden, to_hide):
     sudo_access = pexpect.spawn('sudo steghide embed -cf ' + to_hide + ' -ef ' + to_be_hidden)
-    sudo_access.expect('password', timeout=1000)
+    sudo_access.expect('password', timeout=100)
     sudo_access.sendline('password')
     sudo_access.expect(pexpect.EOF)
-    sudo_access.expect('pass', timeout=1000)
+    sudo_access.expect('pass', timeout=100)
     sudo_access.sendline('carneasada')
     sudo_access.expect(pexpect.EOF)    
 
